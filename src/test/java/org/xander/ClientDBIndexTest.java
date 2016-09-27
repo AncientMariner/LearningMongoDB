c8d33d7780fc9ab7be11fd76b57fb4a5fbf9752e
AncientMariner <xander.lysak@gmail.com>
2016-09-27 10:15:56 +0300

    index creation

---
+++ src/test/java/org/xander/ClientDBIndexTest.java
@@ -0,0 +1,18 @@
+package org.xander;
+
+import org.bson.Document;
+import org.junit.Test;
+
+public class ClientDBIndexTest extends ClientDBAccessTest{
+    @Test
+    public void test() {
+    }
+
+    private void createCompoundIndex() {
+        db.getCollection("restaurants").createIndex(new Document("cuisine", 1).append("address.zipcode", -1));
+    }
+
+    private String createAscendingIndex() {
+        return collection.createIndex(new Document("cuisine", 1));
+    }
+}
