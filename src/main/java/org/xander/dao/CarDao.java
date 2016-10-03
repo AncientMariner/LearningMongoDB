ac55e5571e4bdcd1ec21bc3b2a0f2bbf67c95eb3
AncientMariner <xander.lysak@gmail.com>
2016-10-03 19:58:32 +0300

    more mongo operations

--- src/main/java/org/xander/dao/CarDao.java
+++ src/main/java/org/xander/dao/CarDao.java
@@ -1,32 +1,127 @@
 package org.xander.dao;
 
+import com.mongodb.BulkWriteResult;
+import com.mongodb.CommandResult;
+import com.mongodb.DBCollection;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.BulkOperations;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.ScriptOperations;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 import org.xander.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.project;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.sort;
 
 @Repository
 public class CarDao {
     @Autowired
-    private MongoOperations mongoOperations;
+    private MongoOperations mongoOps;
 
     public void save(Car car) {
-        mongoOperations.save(car);
+        mongoOps.save(car);
     }
 
     public Car get(String id) {
-        return mongoOperations.findOne(Query.query(Criteria.where("id").is(id)), Car.class);
+        return mongoOps.findOne(Query.query(Criteria.where("id").is(id)), Car.class);
     }
 
     public List<Car> getAll() {
-        return mongoOperations.findAll(Car.class);
+        return mongoOps.findAll(Car.class);
     }
 
     public void remove(String id) {
-        mongoOperations.remove(Query.query(Criteria.where("id").is(id)), Car.class);
+        mongoOps.remove(Query.query(Criteria.where("id").is(id)), Car.class);
+    }
+
+    public boolean collectionPresent(String collectionName) {
+        return mongoOps.collectionExists(collectionName);
+    }
+
+    public AggregationResults<ArrayList> aggregate() {
+        Aggregation aggregation = newAggregation(
+                match(Criteria.where("price").lt(60000)),
+//                group("price").count().as("total"),
+                project("price").andExclude("_id"),
+                sort(Sort.Direction.DESC, "price"));
+
+        AggregationResults<ArrayList> aggregate = mongoOps.aggregate(aggregation, Car.class, ArrayList.class);
+        return aggregate;
+    }
+
+    public BulkWriteResult bulkOperation() {
+        Document newDocument1 = new Document("name", "Mercedes")
+                .append("price", 150000);
+
+        Document newDocument2 = new Document("name", "Mercedes AMG")
+                .append("price", 250000);
+
+        BulkWriteResult bulkWriteResult = mongoOps
+                .bulkOps(BulkOperations.BulkMode.ORDERED, "cars")
+                .insert(newDocument1)
+                .insert(newDocument2)
+                .execute();
+        return bulkWriteResult;
+    }
+
+    public long count() {
+        Query query = Query.query(Criteria.where("name").is("Volvo"));
+        return mongoOps.count(query, Car.class);
+    }
+
+    public CommandResult executeCommand() {
+        return mongoOps.executeCommand("{ " + "\"count\" : \"" + "cars" + "\"" + " }");
+    }
+
+    public void executeQuery() {
+//    mongoOps.executeQuery();
+    }
+
+    public void exists() {
+//        mongoOps.exists()
+    }
+
+    public void find() {
+//        mongoOps.find();
+    }
+
+    public void geoNear() {
+//        mongoOps.geoNear();
+    }
+
+    public void getCollection() {
+        String collectionName = mongoOps.getCollectionName(Car.class);
+        DBCollection cars = mongoOps.getCollection(collectionName);
+        Set<String> collectionNames = mongoOps.getCollectionNames();
+    }
+
+    public MongoConverter getMongoConverter() {
+        return mongoOps.getConverter();
+    }
+
+    public void scriptOperations() {
+        ScriptOperations scriptOperations = mongoOps.scriptOps();
+        System.out.println(scriptOperations);
+    }
+
+    public void stream() {
+//        mongoOps.stream();
+    }
+
+    public void mapReduce() {
+//        mongoOps.mapReduce();
     }
 }
--- src/test/java/org/xander/dao/CarDaoTest.java
+++ src/test/java/org/xander/dao/CarDaoTest.java
@@ -1,19 +1,28 @@
 package org.xander.dao;
 
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.CommandResult;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.xander.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @ContextConfiguration(locations = {"classpath:/org/xander/model/applicationContext-dao.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -25,6 +34,7 @@ public class CarDaoTest {
 
     @Before
     public void setUp() {
+        carDao.collectionPresent("cars");
         car = new Car("TestCar", 90000);
         carDao.save(car);
         List<Car> all = carDao.getAll();
@@ -50,6 +60,86 @@ public class CarDaoTest {
             assertNull("car is not null", carDao.get(idToRemove));
         }
     }
+
+    @Test
+    public void testAggregation() {
+        AggregationResults<ArrayList> aggregate = carDao.aggregate();
+        Object result = ((Map.Entry) ((BasicDBObject) ((BasicDBList) aggregate.getRawResults().get("result")).toArray()[2]).entrySet().toArray()[0]).getKey();
+
+        assertThat("price field is not present", result.equals("price"), is(true));
+    }
+
+    @Test
+    public void testCollectionPresent() {
+        assertTrue(carDao.collectionPresent("cars"));
+    }
+
+    @Test
+    public void testBulkOperation() {
+        BulkWriteResult bulkWriteResult = carDao.bulkOperation();
+        assertNotNull(bulkWriteResult);
+        assertThat("there were not 2 documents inserted", bulkWriteResult.getInsertedCount(), is(2));
+    }
+
+    @Test
+    public void testCount() {
+        long count = carDao.count();
+        assertThat("there are more than 1 Volvo cars", count, is(1L));
+
+    }
+
+    @Test
+    public void testExecuteCommand() {
+        CommandResult commandResult = carDao.executeCommand();
+        assertThat("size of the collection is 0", (Integer)commandResult.get("n") > 0, is(true));
+    }
+
+    @Test
+    public void testExecuteQuery() {
+        carDao.executeQuery();
+    }
+
+    @Test
+    public void testExists() {
+        carDao.exists();
+    }
+
+    @Test
+    public void testFind() {
+        carDao.find();
+    }
+
+    @Test
+    public void testGeoNear() {
+        carDao.geoNear();
+    }
+
+    @Test
+    public void testGetCollection() {
+        carDao.getCollection();
+    }
+
+    @Test
+    public void testConverted() {
+        carDao.getMongoConverter();
+
+    }
+
+    @Test
+    public void testScriptOperations() {
+        carDao.scriptOperations();
+    }
+
+    @Test
+    public void testStream() {
+        carDao.stream();
+    }
+
+    @Test
+    public void testMapReduce() {
+        carDao.mapReduce();
+    }
+
 
     @After
     public void tearDown() {
