9938651a1d2c46a6c7d4582ff8417e81337d93ba
AncientMariner <xander.lysak@gmail.com>
2016-11-15 15:57:23 +0200

    updating setter and controller

--- src/main/java/org/xander/model/Car.java
+++ src/main/java/org/xander/model/Car.java
@@ -46,6 +46,10 @@ public class Car {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
